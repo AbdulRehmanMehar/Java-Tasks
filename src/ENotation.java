193ff2cd2469cd938cdd8822b4edec3b1e6f0a36
Abdul Rehman <mehars.6925@gmail.com>
2018-11-25 23:39:22 +0500

    ENOTATION in java

---
+++ src/ENotation.java
@@ -0,0 +1,12 @@
+public class ENotation {
+    public static void main(String [] args){
+        double statement1 = 3.287E6;
+        double statement2 = -9.7865E12; // Returns the same value
+        double statement3 = 7.65491E-3;
+        double statement4 = -153.5432E-5;
+        System.out.println("In java, 3.287E6 is equal to "+ statement1);
+        System.out.println("In java, -9.7865E12 is equal to "+ statement2);
+        System.out.println("In java, 7.65491E-3 is equal to "+ statement3);
+        System.out.println("In java, -153.5432E-5 is equal to "+ statement4);
+    }
+}
