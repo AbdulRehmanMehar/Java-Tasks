f30990f67689d7a58be9b595ce3279209b250f8b
Abdul Rehman <mehars.6925@gmail.com>
2018-11-26 00:33:03 +0500

    Ingredients Calculator

---
+++ src/Ingredients.java
@@ -0,0 +1,9 @@
+public class Ingredients {
+    public static void main(String [] args){
+        double sugar = 1.5,
+            butter = 1,
+            flour = 2.75;
+        int cookies = 48;
+        System.out.println(cookies * sugar + " Cups of Sugar & "+ cookies * butter + " Cups of Butter & " + cookies * flour+ " Cups of Flour are required to make "+ cookies+ " cookies.");
+    }
+}
