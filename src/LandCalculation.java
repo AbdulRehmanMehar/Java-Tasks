61e1f6ddeee479322dd7f5cc85992ecd8eedcb0d
Abdul Rehman <mehars.6925@gmail.com>
2018-11-26 00:12:45 +0500

    Calculates acres in land

---
+++ src/LandCalculation.java
@@ -0,0 +1,8 @@
+public class LandCalculation {
+    public static void main(String [] args){
+        double acre = 43560;
+        double land = 389767;
+        double acresInLand = land / acre;
+        System.out.println("One acre is equal to "+ acre+ " square feet and in a land of "+ land+ " square feet, there are "+ acresInLand + " acres.");
+    }
+}
