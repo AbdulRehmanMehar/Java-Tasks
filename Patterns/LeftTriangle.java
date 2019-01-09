048b53fbfca9cf112f4f28bd4789c376bc3e0bbe
Abdul Rehman <mehars.6925@gmail.com>
2019-01-09 09:22:54 +0500

    Update LeftTriangle.java

--- Patterns/LeftTriangle.java
+++ Patterns/LeftTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RightTriangle{
+public class LeftTriangle{
   public static void main(String [] args){
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt(); 
