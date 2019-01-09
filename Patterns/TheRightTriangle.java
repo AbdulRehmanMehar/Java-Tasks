f761df568d29bedf52ff54758104b136d24503cc
Abdul Rehman <mehars.6925@gmail.com>
2019-01-09 09:22:12 +0500

    The actual right triangle

---
+++ Patterns/TheRightTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TheRightTriangle{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    for(int i=1; i<=N; i++){
+      for(int j=N; j>=i; j--){
+        System.out.print(" ");
+      }
+      for(int j=1; j<=i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
